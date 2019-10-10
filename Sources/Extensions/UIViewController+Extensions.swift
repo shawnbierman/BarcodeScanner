267259372e21b6b52039cecbd501085d929d3c1a
Shawn Bierman <shawn.bierman@mlh.org>
2019-10-10 14:00:32 -0500

    build settings updated

--- Sources/Extensions/UIViewController+Extensions.swift
+++ Sources/Extensions/UIViewController+Extensions.swift
@@ -3,6 +3,7 @@ import UIKit
 extension UIViewController {
   /// A helper function to add child view controller.
   func add(childViewController: UIViewController) {
+//    childViewController.willMove(toParent: self)
     childViewController.willMove(toParent: self)
     addChild(childViewController)
     view.addSubview(childViewController.view)
